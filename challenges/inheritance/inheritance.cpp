a8414e6d75fc42ba96af60b4bf99a3c3a1895893
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 13:42:02 -0300

    inheritance done

--- challenges/inheritance/inheritance.cpp
+++ challenges/inheritance/inheritance.cpp
@@ -20,11 +20,146 @@ public:
 
 // Write your classes here
 
+
+class Dog: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "woof" << std::endl;
+	}
+};
+
+class Cat: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "meow" << std::endl;
+	}
+};
+
+class Bird: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "tweet" << std::endl;
+	}
+};
+
+class Mouse: public Animal
+{
+	void makeSound() const
+	{
+		std::cout << "squeek" << std::endl;
+	}
+};
+class Cow: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "moo" << std::endl;
+	}
+};
+
+class Frog: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "croak" << std::endl;
+	}
+};
+
+class Elephant: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "toot" << std::endl;
+	}
+};
+
+class Duck: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "quack" << std::endl;
+	}
+};
+
+class Fish: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "blub" << std::endl;
+	}
+};
+
+class Seal: public Animal
+{
+	public:
+
+	void makeSound() const
+	{
+		std::cout << "ow ow ow" << std::endl;
+	}
+};
+
+class NotAnAnimal: public Animal
+{
+	private:
+		std::string _name;
+	public:
+	NotAnAnimal(std::string name)
+	: _name(name)
+	{}
+	void makeSound() const
+	{
+		std::cout << _name << " is not an animal" << std::endl;
+	}
+};
+
 // This is the function you need to change in order to instantiate the required
 // classes.
 Animal* Animal::create(const std::string& name)
 {
-	return 0;
+	Animal *an_animal;
+	if(name == "dog")
+		an_animal = new Dog;
+	else if(name == "cat")
+		an_animal = new Cat;
+	else if(name == "bird")
+		an_animal = new Bird;
+	else if(name == "mouse")
+		an_animal = new Mouse;
+	else if(name == "cow")
+		an_animal = new Cow;
+	else if(name == "frog")
+		an_animal = new Frog;
+	else if(name == "elephant")
+		an_animal = new Elephant;
+	else if(name == "duck")
+		an_animal = new Duck;
+	else if(name == "fish")
+		an_animal = new Fish;
+	else if(name == "seal")
+		an_animal = new Seal;
+	else 
+		an_animal = new NotAnAnimal(name);
+	return an_animal;
 }
 
 void makeSound(const std::string& name)
