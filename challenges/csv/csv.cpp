df51788a6568e7f8c850dbe535e82011d4f7e246
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 11:17:33 -0300

    csv done

--- challenges/csv/csv.cpp
+++ challenges/csv/csv.cpp
@@ -4,7 +4,29 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+	string line;
+	int n;
+	
+	getline(cin, line);
+	n = stoi(line);
+	for(int i = 0; i < n; i++){
+		getline(cin, line);
+//		cout << line << endl;
+		istringstream ss(line);
+		vector<string> fields;
+		string f;
+		while(getline(ss, f, ',')){
+			fields.push_back(f);
+			//cout << f << " ";
+		}
+		cout << fields[0] << " is " << fields[4];
+		cout << " years old and lives in " << fields[2] << ", ";
+		cout << fields[1] << "." << endl;
+	}
+
 	return 0;
 }
