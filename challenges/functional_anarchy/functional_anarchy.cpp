b652e8f504c4476d3080e5daf0c71511f9528ee6
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 15:20:28 -0300

    functional_anarchy done

--- challenges/functional_anarchy/functional_anarchy.cpp
+++ challenges/functional_anarchy/functional_anarchy.cpp
@@ -4,7 +4,24 @@
 #include <iterator>
 #include <numeric>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+	vector<int> numbers;
+	int n;
+	
+	cin >> n;
+	while(n--){
+		int number;
+		cin >> number;
+		numbers.push_back(number);
+	}
+
+	sort(numbers.begin(), numbers.end());
+	
+	for(int number : numbers)
+		cout << number << endl;
+		
 	return 0;
 }
