c37d82939a3e6494f22f8cd629e16a357ceb584e
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 03:59:11 -0300

    fibonacci done

--- challenges/fibonacci/fibonacci.cpp
+++ challenges/fibonacci/fibonacci.cpp
@@ -2,5 +2,13 @@
 
 int main(int argc, char** argv)
 {
+	int n;
+	std::cin >> n;
+	long fib[n];
+	fib[0] = 0;
+	fib[1] = 1;
+	for(int i = 2; i <=n ; i++)
+		fib[i] = fib[i-1] + fib[i-2];
+	std::cout << fib[n] << std::endl;
 	return 0;
 }
