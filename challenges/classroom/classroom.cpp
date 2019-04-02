c8cecde736cdc5c1e7ade0816728665f2f3d0adf
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 03:42:29 -0300

    classroom done

--- challenges/classroom/classroom.cpp
+++ challenges/classroom/classroom.cpp
@@ -4,8 +4,46 @@
 #include <map>
 #include <list>
 #include <vector>
+#include <sstream>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	return 0;
+	string line;
+	int n_queries;
+
+	getline(cin, line);
+	istringstream ss(line);
+	ss >> n_queries;
+	vector<string> classroom;
+
+	for(int i = 0; i < n_queries; i++){
+		int query_type, n_times;
+		getline(cin, line);
+		istringstream query(line);
+		query >> query_type;
+		query >> n_times;
+		
+		if(query_type == 1){
+			for(int j = 0; j < n_times; j++){
+				string student;
+				getline(cin, student);
+				classroom.push_back(student);
+				sort(classroom.begin(),classroom.end());
+			}
+		}else if(query_type == 2){
+			for(int j = 0; j < n_times; j++){
+				int key;
+				getline(cin, line);
+				istringstream key_ss(line);
+				key_ss >> key;
+
+				if(key <= classroom.size()){
+					cout << classroom[key - 1] << endl;
+				}
+			}
+		}
+
+	}
 }
