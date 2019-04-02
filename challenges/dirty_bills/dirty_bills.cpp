ffe8a75085c161cf47c086aa10b6f4e25b098682
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 11:30:16 -0300

    dirty_bills done

--- challenges/dirty_bills/dirty_bills.cpp
+++ challenges/dirty_bills/dirty_bills.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+
+	int n;
+	int dirty_count = 0;
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++){
+		int count = 0;
+		string state;
+		cin >> count;
+		cin >> state;
+		if(state == "dirty")
+			dirty_count += count;
+	}
+	
+	if(dirty_count)
+		cout << "There are " << dirty_count << " dirty bills." << endl;
+	else
+		cout << "There are no dirty bills." << endl;
+
 	return 0;
 }
