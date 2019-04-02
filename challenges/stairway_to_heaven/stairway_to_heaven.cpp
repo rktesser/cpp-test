72a961830ef7912699d45583d6a004fdcbfe9d97
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 12:35:04 -0300

    starway_to_heaven done

--- challenges/stairway_to_heaven/stairway_to_heaven.cpp
+++ challenges/stairway_to_heaven/stairway_to_heaven.cpp
@@ -4,5 +4,20 @@
 
 int main(int argc, char** argv)
 {
+	int n;
+	std::cin >> n;
+
+	if(n <= 0){
+		std::cout << "Error." << std::endl;
+		return -1;
+	}
+	for(int i = n; i > 0 ; i--){
+		for(int j = 0; j < i - 1; j++)
+			std::cout << " ";
+		for(int j = i - 1; j < n; j++)
+			std::cout << "#";
+		std::cout << std::endl;
+	}
+
 	return 0;
 }
