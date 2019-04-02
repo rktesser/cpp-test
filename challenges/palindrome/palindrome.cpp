7a2005d65f3dcec2b23902ab227e423bfab324a2
Rafael Keller Tesser <rktesser@inf.ufrgs.br>
2019-04-02 10:38:34 -0300

    palindrome done.

--- challenges/palindrome/palindrome.cpp
+++ challenges/palindrome/palindrome.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
+	int n;
+	string word;
+	
+	cin >> n;
+
+	for(int i = 0; i < n; i++){
+		cin >> word;
+		int wl = word.length();
+		int j;
+		for(j = 0; j < wl && word[j] == word[wl - j - 1]; j++);
+		if(j == wl)
+			cout << word << " is palindrome" << endl;
+		else
+			cout << word << " is not palindrome" << endl;
+	}
+
 	return 0;
 }
